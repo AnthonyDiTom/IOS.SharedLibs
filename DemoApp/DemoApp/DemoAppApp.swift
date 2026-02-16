b04693f5c6f02314a96cce384c1f4a8be2f1b7ef
Anthony Di Tomasso <anthony.ditomasso@portima.com>
2026-02-16 14:50:38 +0100

    add project

---
+++ DemoApp/DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Anthony Di Tomasso on 16/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
